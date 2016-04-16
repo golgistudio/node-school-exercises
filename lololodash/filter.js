1ec21f1b674542ef44ef2e5f65fa03e16f77ae2e
Laurie Reynolds <lauriereynolds@lauries-mbp.attlocal.net>
2016-04-15 20:37:13 -0700

    rename files

--- lololodash/filter.js
+++
@@ -1,9 +0,0 @@
-var _ = require ("lodash");
-
-// Use lodash to filter the users to return only the active users
-var worker = function (users) {
-    return _.filter(users, {'active': true});
-}
-
-module.exports = worker;
-
